9105ad82c411ff9a19b685cee20dba217541a3a3
Christian Porschen <christian.porschen@gmail.com>
2024-08-15 10:30:49 +0200

    wip converter last import

--- converter/strong-app.py
+++ converter/strong-app.py
@@ -15,18 +15,33 @@ def augment_row(row, exercises):
     return row
 
 
-def converter():
+def filter_imports(data):
+    """Exclude exercises that are already imported in the database, based on the last import date found in import.json"""
+    # get last import date
+    last_import_date = json.load(open('data/import.json'))['last_import_date']
+    # get all data rows that have been imported since the last import date
+    data = data[data['Date'] > last_import_date]
+    # write todays date to import.json
+    json.dump({'last_import_date': pd.Timestamp.now().strftime('%Y-%m-%d %H:%M:%S')},
+              open('data/import.json', 'w'))
+    return data
+
+
+def converter(apply_filter=False):
     data = pd.read_csv('data/strong.csv')
     # import exercise json
     exercises = json.load(open('data/exercises.json'))
+    if apply_filter is True:
+        data = filter_imports(data)
     # split exercise name and equipment
     data['exercise'], data['equipment'] = zip(
         *data['Exercise Name'].map(strip_appendix))
     data.drop(columns=["Workout Name", "Duration"], inplace=True)
     # augment data with exercise data
     data = data.apply(augment_row, axis=1, exercises=exercises)
-    # export data
-    data.to_csv('data/strong_augmented.csv', index=False)
+    # export data with date of today
+    data.to_csv(
+        f'data/strong{pd.Timestamp.today().strftime('%Y-%m-%d')}.csv', index=False)
 
 
 if __name__ == '__main__':
