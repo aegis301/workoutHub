e7334555a65e6729b65ebd9921efb8b8833556b9
aegis301 <christian.porschen@gmail.com>
2024-08-20 07:02:04 +0200

    remove legacy code

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,7 @@
-import models.models as models
 from fastapi import FastAPI
-from database import engine, SessionLocal
-from sqlalchemy.orm import Session
 
 
 app = FastAPI()
-models.Base.metadata.create_all(bind=engine)
-
-
-db_dependency = Annotaded(Session, Depends(get_db))
 
 
 @app.get("/")
