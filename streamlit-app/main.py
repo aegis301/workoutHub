0cd6c8d70f6a1d839b587344907ca359891a7152
aegis301 <christian.porschen@gmail.com>
2024-10-23 20:59:52 +0200

    switch order

--- streamlit-app/main.py
+++ streamlit-app/main.py
@@ -10,9 +10,6 @@ sets = convert_response_data_to_df(sets_response)
 
 st.title('workoutHub')
 
-# Display the data
-st.write(sets)
-
 # Create a pie chart of sets by primary muscle group using Plotly
 fig1 = px.pie(sets, names='primary_muscle_group_name', title='Sets by Primary Muscle Group')
 # create a pie chart of sets by main muscle group using Plotly
@@ -21,3 +18,6 @@ fig2 = px.pie(sets, names='main_muscle_group', title='Sets by Main Muscle Group'
 
 st.plotly_chart(fig1)
 st.plotly_chart(fig2)
+
+# Display the data
+st.write(sets)
