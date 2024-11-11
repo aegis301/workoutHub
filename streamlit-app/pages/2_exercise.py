040ee48552bb3ff39c894d270d3da4a266eee28f
aegis301 <christian.porschen@gmail.com>
2024-11-11 15:23:30 +0100

    queries for sets by exercise and equipment

--- streamlit-app/pages/2_exercise.py
+++ streamlit-app/pages/2_exercise.py
@@ -9,29 +9,47 @@ st.set_page_config(
 st.title("Exercise")
 
 exercises = query_with_cache('http://localhost:8000/exercises/')
+equipment = query_with_cache('http://localhost:8000/equipment/')
 
 if "sets_per_exercise" not in st.session_state:
     st.session_state.sets_per_exercise = {}
 
 
-def query_exercise(option):
-    if option is None:
+def query_exercise(e_selection, eq_selection):
+    if e_selection is None:
         st.warning("Please select an exercise.")
         return {}
-    if option not in st.session_state.sets_per_exercise:
-        st.session_state.sets_per_exercise[option] = query_with_cache('http://localhost:8000/exercises/' + option)
-    return st.session_state.sets_per_exercise[option]
+    st.session_state.exercise_selection = e_selection
+    # find exercise id
+    exercise_option = exercises[exercises.name == e_selection].id.values[0]
+
+    if eq_selection != "All":
+        equipment_option = equipment[equipment.name == eq_selection].id.values[0]
+        query = f"http://localhost:8000/sets/exercises/equipment/{exercise_option}/{equipment_option}"
+    else:
+        query = f'http://localhost:8000/sets/exercises/{exercise_option}'
+    if exercise_option not in st.session_state.sets_per_exercise:
+        st.session_state.sets_per_exercise = query_with_cache(query)
+    return st.session_state.sets_per_exercise
 
 
 with st.form(key="exercise_form"):
-    option = st.selectbox(
+    exercise_selection = st.selectbox(
         "Exercise",
-        exercises.name.values if exercises is not None else [],
+        exercises.name if exercises is not None else [],
         index=None,
         placeholder="Select an exercise...",
     )
+    equipment_options = ["All"] + (equipment.name.tolist() if equipment is not None else [])
+    equipment_selection = st.selectbox(
+        "Equipment",
+        equipment_options,
+        index=0,
+        placeholder="Select an equipment...",
+    )
+    submit_button = st.form_submit_button(label="Submit")
 
-    if st.form_submit_button("Submit"):
-        query_exercise(option)
 
-st.write(st.session_state.sets_per_exercise)
+if submit_button:
+    sets = query_exercise(exercise_selection, equipment_selection)
+    st.write(st.session_state.sets_per_exercise)
